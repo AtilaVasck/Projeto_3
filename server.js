3ed56276b1ffd3e7399fa29d35d83a93b60a84f9
AtilaVasck <atilacleiton52@gmail.com>
2024-07-08 13:32:38 -0300

    add all rotes add route 404

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 import {createServer} from 'node:http';
 import fs from "node:fs";
 
+import lerDadosReceita from './helper/lerReceitas';
+const receitas = [];
 const PORT = 3333;
 
 const server = createServer((request, response)=>{
     const {url, method} = request;
 
     if( method === 'GET' && url === "/receitas"){
+        lerDadosReceita((err, receitas)=>{
 
+        if(err){response.writeHead(500, {"Content-Type" : "application/json"});
+        response.end (JSON.stringify({message: "Erro ao ler dados"}));
+        return
+        }
+        response.writeHead(200, {"Content-Type" : "application/json"});
+        response.end(JSON.stringify(receitas));    
+    })
     }else if( method === 'POST' && url === '/receitas'){
 
     }else if( method === 'GET' && url.startsWith('/receitas/')){
