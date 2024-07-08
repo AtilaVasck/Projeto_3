d018c8f1d2a741aa10376784f5c1207d037efda2
AtilaVasck <atilacleiton52@gmail.com>
2024-07-08 14:33:42 -0300

    Esse é o commit que o Carlos concertou

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 import {createServer} from 'node:http';
 import fs from "node:fs";
 import { v4 as uuidv4 } from 'uuid';
+import { URLSearchParams } from 'node:url';
 
-import lerDadosReceita from './helper/lerReceitas';
+import lerDadosReceita from './helper/lerReceitas.js';
 
 const PORT = 3333;
 
@@ -16,7 +17,7 @@ const server = createServer((request, response)=>{
             response.writeHead(500, {"Content-Type" : "application/json"});
             response.end (JSON.stringify({message: "Erro ao ler dados"}));
             return
-            
+
         }
         response.writeHead(200, {"Content-Type" : "application/json"});
         response.end(JSON.stringify(receitas));    
@@ -55,9 +56,10 @@ const server = createServer((request, response)=>{
     }else if( method === 'DELETE' && url.startsWith('/receitas/')){
 
     }else if( method === 'GET' && url.startsWith('/categorias/')){
-
+        //localhost:3333/categoria/prato principal
+        const categoria = url.split("/")[2];
     }else if( method === 'GET' && url.startsWith('/busca')){
-
+        //localhost:3333/busca?termo=cebola
     }else if( method === 'GET' && url.startsWith('/ingredientes')){
 
     }else{
